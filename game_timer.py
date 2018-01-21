54117d86a81fe2cdf1a768971089d8380b898909
Kaaii <something.kai@gmail.com>
2018-01-21 02:35:48 -0800

    Removed idle status detection since it wasn't working.

--- game_timer.py
+++ game_timer.py
@@ -24,27 +24,16 @@ class GameTimer:
         self.gamer = discord.User
         self.start_time = 0
         self.ptime = 0 #keeps count of playtime
-        self.status = 'busy'
-
-    async def on_member_update(self,before,after):
-        """
-        Checks if user is idle, or was idle.
-        Then changes notification settings appropriately.
-        """
-        if after.status == idle:
-            self.status = 'free'
-            #print('Status changed to active.')
-        if before.status == idle:
-            self.status = 'busy'
-            #print('Status changed to busy again.')
+        self.toggle = 'busy'
 
     async def on_member_update(self, before, after):
         """
         Event listener that waits for a member to change status to 'Playing.'
         Will then wait to message user reminders not to play too long.
         """
+
         self.gamer = after
-        if after.game != None and after.status != idle:
+        if after.game != None:
             await self.bot.send_message(after,"You have started playing {}".format(after.game))
             await self.bot.send_message(after,"I'll let you know when to take a break.")
         self.start_time = time.time()
@@ -52,8 +41,10 @@ class GameTimer:
         #Begins timers.
         h=0
         t=3600 #t=30 for testing (30s intervals)
-        while after.game != None and self.status == 'busy':
+        while after.game != None:
             h += 1
+            if self.toggle == 'free':
+                break
             await asyncio.sleep(t)
             await self.bot.send_message(after, after.mention + " You've been playing for {} hour(s).".format(h))
             if h == 1: #One hour reminder.
@@ -76,26 +67,29 @@ class GameTimer:
         h = int(self.ptime/3600) #hours
         m = int(self.ptime/60) #minutes
         s = int((self.ptime%3600)%60) #seconds
-        if self.gamer.game != None:
-            await self.bot.say("You've been playing {} for: {}h {}m {}s.".
-                format(self.gamer.game,h,m,s))
-        elif self.gamer.game == None:
-            await self.bot.say("You haven't been playing anything for: {}h {}m {}s.".
-                format(h,m,s))
+        try:
+            if self.gamer.game != None:
+                await self.bot.say("You've been playing {} for: {}h {}m {}s.".
+                    format(self.gamer.game,h,m,s))
+            elif self.gamer.game == None:
+                await self.bot.say("You haven't been playing anything for: {}h {}m {}s.".
+                    format(h,m,s))
+        except:
+            print("No status change detected. Please restart the game.")
 
     @commands.command()
     async def free(self):
         """
         Lets the bot know you are free to game without reminders.
         """
-        self.status = 'free'
+        self.toggle = 'free'
 
     @commands.command()
     async def busy(self):
         """
         Lets the bot know you are busy again.
         """
-        self.status = 'busy'
+        self.toggle = 'busy'
 
 def setup(bot):
     bot.add_cog(GameTimer(bot))
